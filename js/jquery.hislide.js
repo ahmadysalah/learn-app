86462e1ff9c2d7f1f0d3a072f2907323bd00c3cc
ahmadysalah <a.ahmad.salah@gmail.com>
2021-08-07 15:18:12 +0300

    add text to slider

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -11,13 +11,13 @@
 
         $.extend(true, setting, options);
         var states = [
-            { $zIndex: 1, width: 120, height: 150, top: 69, left: -100, $opacity: 0.2 },
-            { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.4 },
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 0.7 },
-            { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1 },
-            { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
-            { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4 },
-            { $zIndex: 1, width: 120, height: 150, top: 69, left: 740, $opacity: 0.2 }
+            { $zIndex: 1, width: 120, height: 150, top: 69, left: -100, $opacity: 0.2, $opacityText: 0 },
+            { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.4, $opacityText: 0 },
+            { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 0.7, $opacityText: 0 },
+            { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1, $opacityText: 1 },
+            { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7, $opacityText: 0 },
+            { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4, $opacityText: 0 },
+            { $zIndex: 1, width: 120, height: 150, top: 69, left: 740, $opacity: 0.2, $opacityText: 0 }
         ];
 
         var $lis = $ele.find('li');
@@ -44,7 +44,8 @@
         function move() {
             $lis.each(function (index, element) {
                 var state = states[index];
-                $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+                $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity)
+                $(element).css('zIndex', state.$zIndex).find('p').css('opacity', state.$opacityText)
             });
         }
 
